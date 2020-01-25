c3cc8a6c6a0b5e3f6b4305d086c114084571e000
Mr-Npc <mr.npc@outlook.com>
2020-01-25 13:33:04 +0800

    后来登录拦截

---
+++ src/main/java/pub/npc/soft/erp/interceptor/admin/LoginInterceptor.java
@@ -0,0 +1,28 @@
+package pub.npc.soft.erp.interceptor.admin;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String requesURL = httpServletRequest.getRequestURI();
+        Object user = httpServletRequest.getSession().getAttribute("user");
+        System.out.print("链接"+requesURL+"进入拦截器！");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
