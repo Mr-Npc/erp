7a1bf9984e7b2c1856a590abe340825ef849fe81
Mr-Npc <mr.npc@outlook.com>
2020-02-12 04:34:35 +0800

    登录拦截 增加ajax拦截 增加普通链接跳转拦截 spring-mvc.xml拦截更新 增加菜单管理控制器

--- src/main/java/pub/npc/soft/erp/controller/admin/MenuController.java
+++ src/main/java/pub/npc/soft/erp/controller/admin/MenuController.java
@@ -1,8 +1,15 @@
 package pub.npc.soft.erp.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import pub.npc.soft.erp.entity.admin.Menu;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 菜单管理控制器
@@ -12,9 +19,39 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/admin/menu")
 public class MenuController {
 
-    @RequestMapping("/list")
+    @RequestMapping(value = "/list" ,method = RequestMethod.GET)
     public ModelAndView list(ModelAndView modelAndView){
         modelAndView.setViewName("menu/list");
         return modelAndView;
+    }
+
+    /**
+     * 添加菜单
+     * @param menu
+     * @return
+     */
+    @RequestMapping(value = "/add" ,method = RequestMethod.POST)
+    @ResponseBody  //返回json字符串，不加的话直接查找视图
+    public Map<String, String> add(Menu menu){
+        Map<String,String> map = new HashMap<String, String>();
+        if(menu == null){
+            map.put("type","error");
+            map.put("msg","请填写正确的菜单名称");
+            return map;
+        }
+        if(StringUtils.isEmpty(menu.getName())){
+            map.put("type","error");
+            map.put("msg","请填写菜单名称");
+            return map;
+        }
+        if(StringUtils.isEmpty(menu.getIcon())){
+            map.put("type","error");
+            map.put("msg","请选择菜单图标");
+            return map;
+        }
+        map.put("typr","success");
+        map.put("msg","添加成功");
+
+        return map;
     }
 }
